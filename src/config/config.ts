51feab55f1557244b3900b998a4dde2bab4b93cb
Leb <eniolaolagbegi@gmail.com>
2025-04-07 10:01:03 +0100

    update

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,5 +7,13 @@ const config = {
     PORT: process.env.PORT as string,
     ACCESS_SECRETKEY: process.env.ACCESS_SECRETKEY as string
 }
+const requiredEnvVars = ['PORT', 'ACCESS_SECRETKEY'];
+const missingEnvVars = requiredEnvVars.filter((key) => !process.env[key]);
+
+if (missingEnvVars.length > 0) {
+    console.error('Missing required environment variables:', missingEnvVars);
+}
+
+console.table(config);
 
 export default config
